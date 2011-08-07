bd815d254cff36e0fcb1dd97a06c48e3dd170109
surya <surya.gaddipati@gmail.com>
2011-08-07 18:17:41 -0400

    new example

--- spec/linq_spec.rb
+++ spec/linq_spec.rb
@@ -1,12 +1,13 @@
 require 'linqr'
 describe "linqr" do
   it "should apply selector" do 
+    numbers = [ 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 ]
     output =  _{ 
       from x
-      in_ [1,2,3]
+      in_ numbers
       where x > 1
       select x * 3
     }
-    output.should == [6,9]
+    output.should == [15, 12, 9, 27, 24, 18, 21, 6]
   end
 end
