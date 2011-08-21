ec249a8cb71f27e13aa6d44a3f57be7067c19add
surya <surya.gaddipati@gmail.com>
2011-08-21 16:30:01 -0500

    Added support for complex binary expression syntax

--- lib/linqr.rb
+++ lib/linqr.rb
@@ -2,16 +2,42 @@ require 'sourcify'
 require 'ripper'
 require 'awesome_print'
 require 'ripper2ruby'
+class Ruby::Variable
+  def exp_value(binding)
+    binding.eval(self.to_s)
+  end
+end
+class Ruby::Integer
+  def exp_value(binding)
+   self.value
+  end
+end
+class Ruby::Statements
+  def exp_value(binding)
+    binary_exp = elements.first
+    right_val = binary_exp.right.exp_value(binding)
+    left_val = binary_exp.left.exp_value(binding)
+    left_val.send(binary_exp.operator.to_ruby.to_sym, right_val)
+  end
+end
+
 class Object
   def _(&proc_exp)
+    @binding = proc_exp.binding
     exp = Ripper::RubyBuilder.build(proc_exp.to_source)
-
     source_name = fcall(exp,"in_").select(Ruby::Arg).first.arg.token
-    source = proc_exp.binding.eval(source_name)
+    source = @binding.eval(source_name)
 
+    variable = fcall(exp,"from").arguments.first.arg.to_s
+    
     where_exp =  fexp(exp,"where")
     #assuming to be a binary expression
-    filtered_values = source.select {|e| e.send(where_exp.operator.to_ruby.to_sym, where_exp.right.value)}
+    filtered_values = source.select do|e| 
+      Object.send(:define_method,variable.to_sym) { e }
+      left_val = evaluate(where_exp.left)
+      right_val = evaluate(where_exp.right)
+      left_val.send(where_exp.operator.to_ruby.to_sym, right_val)
+    end
 
     
     select_exp =  fexp(exp,"select")
@@ -19,6 +45,11 @@ class Object
     filtered_values.collect{|e| e.send(select_exp.operator.to_ruby.to_sym, select_exp.right.value) }
 
   end
+
+  def evaluate(exp)
+    exp.exp_value(@binding)
+  end
+  
   def fcall(exp, fname)
     exp.select(Ruby::Call).select {|call| call.token == fname}.first
   end
--- spec/linq_spec.rb
+++ spec/linq_spec.rb
@@ -1,6 +1,6 @@
 require 'linqr'
 describe "linqr" do
-  it "should apply selector" do 
+  it "simple binary expression" do 
     numbers = [ 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 ]
     output =  _{ 
       from x
@@ -9,5 +9,15 @@ describe "linqr" do
       select x * 3
     }
     output.should == [15, 12, 9, 27, 24, 18, 21, 6]
+  end
+  it"a little complex binary expression"do 
+    numbers = [ 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 ]
+    output =  _{ 
+      from x
+      in_ numbers
+      where (x % 2) > 0
+      select x * 1
+    }
+    output.should == [ 5, 1, 3, 9, 7 ]
   end
 end
